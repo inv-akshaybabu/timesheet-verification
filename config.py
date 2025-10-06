546310a3e29a26c672a6a18aa628a3d55d5c1655
bismillakhan.s <bismillakhan.s@innovaturelabs.com>
2025-10-06 10:07:00 +0530

    feat : cache persist added

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # Single Google Sheet ID containing all engineer sheets
-MAIN_SPREADSHEET_ID = '1VZx2w_IiT5UXyg3CLk9uXmzAjshj_PDa6BlGhUtMwjA'  # Replace with your main sheet ID
+MAIN_SPREADSHEET_ID = '1YU1bE7tC6pL6dKld6zYQ8EKXrU2HxCvVolO2ucJBbOc'  # Replace with your main sheet ID
 
 # Engineer names (these will be used as sheet names within the main spreadsheet)
 ENGINEER_NAMES = [
